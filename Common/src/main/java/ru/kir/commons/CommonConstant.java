5339deae42e1b7c728466dd33177c4136f0388a6
KirillPamPam <kirill.mai308@gmaio.com>
2015-11-08 23:25:36 +0300

    Add common class

---
+++ Common/src/main/java/ru/kir/commons/CommonConstant.java
@@ -0,0 +1,11 @@
+package ru.kir.commons;
+
+/**
+ * Created by Kirill Zhitelev on 08.11.2015.
+ */
+public class CommonConstant {
+    public static final String TEXT = "Text";
+    public static final String THEME = "Theme";
+    public static final String DATE = "Date";
+    public static final String ID = "Id";
+}

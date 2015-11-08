ece7f57cd43f6708e9c4006ea6e5afc70d3ca58e
KirillPamPam <kirill.mai308@gmaio.com>
2015-11-08 17:37:20 +0300

    Delete DAO

--- ServerDiary/src/main/java/ru/kir/server/database/dao/DiaryDeleteDao.java
+++
@@ -1,8 +0,0 @@
-package ru.kir.server.database.dao;
-
-/**
- * Created by Kirill Zhitelev on 28.10.2015.
- */
-public interface DiaryDeleteDao {
-    void deleteRecord();
-}
--- ServerDiary/src/main/java/ru/kir/server/database/dao/DiaryUpdateDao.java
+++
@@ -1,8 +0,0 @@
-package ru.kir.server.database.dao;
-
-/**
- * Created by Kirill Zhitelev on 07.11.2015.
- */
-public interface DiaryUpdateDao {
-    void updateRecord(String text);
-}
